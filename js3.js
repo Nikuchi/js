8653117f9e2e60ba4bf8a76f6f522121ff988f95
Nikuchi <ponomarenkon963@gmail.com>
2021-06-29 14:14:29 +0400

    Добавлены задания по регулярки

---
+++ js3.js
@@ -0,0 +1,18 @@
+//1
+/*let str = "ahb acb aeb aeeb adcb axeb";
+let result = str.match(/a[a-z]b/g); 
+console.log( result );  */   
+//2
+/*
+let str = "a.a aba aea";
+let result = str.match(/a[.]a/g); 
+console.log( result ); */
+//3
+/*
+let str = "aa aba abba abbba abca abea";
+let result = str.match(/ab?b?b?a/g); 
+console.log( result ); */
+//4
+/*
+let  str  =  'aba accca azzza wwwwa' ;
+console.log(str.replace (/a.+?a/g, '!'));*/
